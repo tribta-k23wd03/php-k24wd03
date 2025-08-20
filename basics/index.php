a22fd6992a36e539431d4ddf09fbbdef8846e6c2
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-08-20 14:12:27 +0700

    update

---
+++ basics/index.php
@@ -0,0 +1,11 @@
+<?php
+// NOTE: Namespace syntax
+
+// Khởi tạo namespace:
+// namespace Name\Module\Group;
+
+// Sử dụng namespace
+// use Name\Module\Group;
+
+// Sử dụng namespace & tạo alias
+// use Name\Module\Group as alias_name;
