e2db08134f8a75fd1ec248450c99f203e9066abe
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-08-25 17:07:58 +0700

    update name cho route get "/login"
    Route::get("/login", [AuthenticatedSessionController::class, "create"])->name("login");

--- laravel/myBank/routes/auth.php
+++ laravel/myBank/routes/auth.php
@@ -7,10 +7,10 @@ use Inertia\Inertia;
 
 Route::middleware("guest")->group(function () {
 
-    Route::get("/login", [AuthenticatedSessionController::class, "create"]);
+    Route::get("/login", [AuthenticatedSessionController::class, "create"])->name("login");
     Route::post("/login", [AuthenticatedSessionController::class, "store"]);
 
-    Route::get("/register", [RegisteredUserController::class, "create"]);
+    Route::get("/register", [RegisteredUserController::class, "create"])->name("register");
     Route::post("/register", [RegisteredUserController::class, "store"]);
 });
 
