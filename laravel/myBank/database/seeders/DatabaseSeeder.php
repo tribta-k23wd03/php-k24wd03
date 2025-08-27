b1fd38378c1c5f1e2a1efe896fd730d1a3e53f0a
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-08-27 15:19:27 +0700

    update

--- laravel/myBank/database/seeders/DatabaseSeeder.php
+++ laravel/myBank/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        DB::truncate();
+        User::truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         User::factory(10)->create();
