5e1c3fd36a467730fae82e0f977b2ff71b9dcff5
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-08-29 18:00:53 +0700

    update

---
+++ laravel/myChat/resources/js/routes/chat/index.ts
@@ -0,0 +1,84 @@
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../wayfinder'
+/**
+* @see \App\Http\Controllers\ConversationController::index
+ * @see app/Http/Controllers/ConversationController.php:12
+ * @route '/chat'
+ */
+export const index = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: index.url(options),
+    method: 'get',
+})
+
+index.definition = {
+    methods: ["get","head"],
+    url: '/chat',
+} satisfies RouteDefinition<["get","head"]>
+
+/**
+* @see \App\Http\Controllers\ConversationController::index
+ * @see app/Http/Controllers/ConversationController.php:12
+ * @route '/chat'
+ */
+index.url = (options?: RouteQueryOptions) => {
+    return index.definition.url + queryParams(options)
+}
+
+/**
+* @see \App\Http\Controllers\ConversationController::index
+ * @see app/Http/Controllers/ConversationController.php:12
+ * @route '/chat'
+ */
+index.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
+    url: index.url(options),
+    method: 'get',
+})
+/**
+* @see \App\Http\Controllers\ConversationController::index
+ * @see app/Http/Controllers/ConversationController.php:12
+ * @route '/chat'
+ */
+index.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
+    url: index.url(options),
+    method: 'head',
+})
+
+    /**
+* @see \App\Http\Controllers\ConversationController::index
+ * @see app/Http/Controllers/ConversationController.php:12
+ * @route '/chat'
+ */
+    const indexForm = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+        action: index.url(options),
+        method: 'get',
+    })
+
+            /**
+* @see \App\Http\Controllers\ConversationController::index
+ * @see app/Http/Controllers/ConversationController.php:12
+ * @route '/chat'
+ */
+        indexForm.get = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+            action: index.url(options),
+            method: 'get',
+        })
+            /**
+* @see \App\Http\Controllers\ConversationController::index
+ * @see app/Http/Controllers/ConversationController.php:12
+ * @route '/chat'
+ */
+        indexForm.head = (options?: RouteQueryOptions): RouteFormDefinition<'get'> => ({
+            action: index.url({
+                        [options?.mergeQuery ? 'mergeQuery' : 'query']: {
+                            _method: 'HEAD',
+                            ...(options?.query ?? options?.mergeQuery ?? {}),
+                        }
+                    }),
+            method: 'get',
+        })
+    
+    index.form = indexForm
+const chat = {
+    index,
+}
+
+export default chat
