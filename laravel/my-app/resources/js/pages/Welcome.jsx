6624142a427c9c9e83a3b83086a2b9d34e0d5e62
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-08-29 13:39:46 +0700

    update

--- laravel/my-app/resources/js/pages/Welcome.jsx
+++
@@ -1,5 +0,0 @@
-function welcome() {
-    return <h1 style={{ color: 'purple', fontFamily: 'intel one mono' }}>Welcome to my Laravel Application number 01.</h1>;
-}
-
-export default welcome;
--- laravel/my-app/routes/counter.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\CounterController;
-use Illuminate\Support\Facades\Route;
-
-Route::get("/counter", [CounterController::class, 'show'])->name('counter.show');
-Route::post("/counter", [CounterController::class, 'update'])->name('counter.update');
