39e0454afb4999be3c379b893950e84619ec9106
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-08-29 15:06:58 +0700

    update

--- laravel/myChat/app/Models/Message.php
+++ laravel/myChat/app/Models/Message.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
-    //
+    public function messages(): BelongsTo
+    {
+        return $this->belongsTo(Conversation::class);
+    }
 }
