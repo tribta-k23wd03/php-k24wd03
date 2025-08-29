12be5524170acade517d392fe3b4845a3b5c82af
Gabriel7721 <216126488+tribta-k23wd03@users.noreply.github.com>
2025-08-29 14:48:04 +0700

    Update 2025_08_29_074228_create_conversations_table.php

--- laravel/myChat/database/migrations/2025_08_29_074228_create_conversations_table.php
+++ laravel/myChat/database/migrations/2025_08_29_074228_create_conversations_table.php
@@ -14,6 +14,15 @@ return new class extends Migration
         Schema::create('conversations', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            $table->string('name')->nullable();
+            $table->boolean('is_direct')->default(true);
+
+            $table->foreignId('created_by')
+                ->constrained('users')
+                ->cascadeOnDelete();
+
+            $table->index(['is_direct', 'updated_at']);
         });
     }
 
